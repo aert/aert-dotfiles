e8ae2e9a96e29df63df67bb5cd623965cc08ffbf
abdourakhmane <dev.aert@gmail.com>
2020-04-22 00:13:44 +0000

    emacs: fuzzy matching + autoformat

--- user-config/doom.d/init.el
+++ user-config/doom.d/init.el
@@ -22,7 +22,7 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -52,6 +52,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
+       format            ; automated prettiness
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
