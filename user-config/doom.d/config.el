62a3a65beea982abf1d544af67e274cb2543ba03
abdourakhmane <dev.aert@gmail.com>
2020-04-28 18:22:23 +0000

    emacs

--- user-config/doom.d/config.el
+++ user-config/doom.d/config.el
@@ -24,7 +24,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-Iosvkem)
+(setq doom-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -54,6 +54,7 @@
 
 (setq doom-modeline-project-detection 'truncate-with-project)
 
+(setq deft-directory "~/org")
 
 ;; My Functions ................................................................
 
