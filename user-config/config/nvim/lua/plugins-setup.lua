c1fb34b53e623dcfbc32765a0055893fa549d14b
abdourakhmane <ripault.a@gmail.com>
2021-11-27 15:52:28 +0000

    vim: incremetal_selection

--- user-config/config/nvim/lua/plugins-setup.lua
+++ user-config/config/nvim/lua/plugins-setup.lua
@@ -4,6 +4,15 @@ require'nvim-treesitter.configs'.setup {
         enable = true,              -- false will disable the whole extension
         disable = { "html" }
     },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = '<CR>',
+            scope_incremental = '<CR>',
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>',
+        },
+    },
 }
 
 require("which-key").setup {
