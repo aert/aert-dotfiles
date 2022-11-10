de61771fe130dde94379b8923461650993ab2878
abdourakhmane <ripault.a@gmail.com>
2022-11-10 14:00:13 +0000

    emacs: cleanup themes

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -248,11 +248,12 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         solarized-wombat-dark
+                         ir-black
                          gruvbox-dark-soft
                          gruvbox-light-soft
                          zenburn
                          solarized-zenburn
-                         doom-solarized-light
                          gruvbox-dark-soft
                          spacemacs-dark
                          solarized-gruvbox-light
