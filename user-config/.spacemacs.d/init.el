9eaaa71eea7591bb619113d110e42a7cd6f775f5
abdourakhmane <ripault.a@gmail.com>
2023-05-31 11:53:34 +0000

    emacs: modus themes

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -52,7 +52,7 @@ This function should only modify configuration layer settings."
      lsp
      imenu-list
      markdown
-     ;; themes-megapack
+     themes-megapack
      ;; multiple-cursors
      (org :variables
           org-enable-roam-support t
@@ -256,6 +256,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         modus-operandi-tinted
+                         modus-vivendi-deuteranopia
                          apropospriate-light
                          apropospriate-dark
                          moe-light
