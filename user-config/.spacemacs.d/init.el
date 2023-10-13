b43335c420b9fb199db4f8d2778110b52386245c
abdourakhmane <ripault.a@gmail.com>
2023-10-13 18:19:45 +0000

    emacs: font

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -291,9 +291,15 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("Monaco"
-                               :size 16.0
+   dotspacemacs-default-font '("OperatorMono Nerd Font"
+                               :size 15.0
                                :weight normal)
+   ;; dotspacemacs-default-font '("CaskaydiaCove NF"
+   ;;                             :size 15.0
+   ;;                             :weight normal)
+   ;; dotspacemacs-default-font '("Monaco"
+   ;;                             :size 16.0
+   ;;                             :weight normal)
    ;; dotspacemacs-default-font '("Rec Mono Duotone"
    ;;                             :size 16.0
    ;;                             :weight normal
