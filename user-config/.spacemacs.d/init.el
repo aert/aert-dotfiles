6a8f5c6816d94a057c921494cce6006ee31e634e
abdourakhmane <ripault.a@gmail.com>
2023-10-14 15:24:19 +0000

    emacs: theme

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -262,18 +262,8 @@ It should only modify the values of Spacemacs settings."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
                          alect-black-alt
-                         chocolate
-                         eziam-dark
-                         eziam-light
-                         majapahit-dark
-                         zenburn
-                         ir-black
-                         apropospriate-light
-                         apropospriate-dark
-                         moe-light
-                         moe-dark
-                         kaolin-light
-                         kaolin-eclipse)
+                         alect-light
+                         chocolate)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
    ;; `all-the-icons', `custom', `doom', `vim-powerline' and `vanilla'. The
