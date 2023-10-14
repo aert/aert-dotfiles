255c5e6164a8b8e3e131a46e48cc2e88fdc1c081
abdourakhmane <ripault.a@gmail.com>
2023-10-14 15:21:50 +0000

    emacs: jetbrains font

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -291,9 +291,12 @@ It should only modify the values of Spacemacs settings."
    ;; Default font or prioritized list of fonts. The `:size' can be specified as
    ;; a non-negative integer (pixel size), or a floating-point (point size).
    ;; Point size is recommended, because it's device independent. (default 10.0)
-   dotspacemacs-default-font '("OperatorMono Nerd Font"
+   dotspacemacs-default-font '("JetBrains Mono"
                                :size 15.0
                                :weight normal)
+   ;; dotspacemacs-default-font '("OperatorMono Nerd Font"
+   ;;                             :size 15.0
+   ;;                             :weight normal)
    ;; dotspacemacs-default-font '("CaskaydiaCove NF"
    ;;                             :size 15.0
    ;;                             :weight normal)
