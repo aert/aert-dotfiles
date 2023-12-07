10339b2ec4a8ee9c7f5371010d590cf3bb6cfdf1
abdourakhmane <ripault.a@gmail.com>
2023-12-07 17:15:48 +0000

    emacs: themes

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -271,6 +271,7 @@ It should only modify the values of Spacemacs settings."
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
                          doom-nord
+                         doom-earl-grey
                          alect-black-alt
                          moe-light
                          alect-light
