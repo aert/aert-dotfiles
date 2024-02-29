29126fcb2f683fd4d1f8a701c2274d6b76006df7
abdourakhmane <ripault.a@gmail.com>
2024-02-29 13:27:44 +0000

    lazyvim

---
+++ user-config/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+-- https://www.lazyvim.org/configuration/recipes#make-tokyonight-transparent
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
