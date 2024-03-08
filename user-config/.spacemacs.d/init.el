d150f640e80f3390466c678ab9808d38441c9b1e
abdourakhmane <ripault.a@gmail.com>
2024-03-08 17:22:34 +0000

    lazyvim

--- user-config/.spacemacs.d/init.el
+++ user-config/.spacemacs.d/init.el
@@ -270,6 +270,8 @@ It should only modify the values of Spacemacs settings."
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         gruvbox-dark-medium
+                         kaolin-aurora
                          doom-palenight
                          doom-nord
                          doom-earl-grey
