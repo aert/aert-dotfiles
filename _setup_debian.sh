522d7ff1a90ad1c10082c07f003f0bb186b2cd2f
abdourakhmane <ripault.a@gmail.com>
2024-06-06 20:49:53 +0000

    lazyvim, xclip

--- _setup_debian.sh
+++ _setup_debian.sh
@@ -3,9 +3,9 @@
 set -ex
 
 # https://medium.com/opsops/how-to-disable-packagekit-f935207044c1
-sudo dpkg-divert --divert /etc/PackageKit/20packagekit.distrib --rename  /etc/apt/apt.conf.d/20packagekit
+sudo dpkg-divert --divert /etc/PackageKit/20packagekit.distrib --rename /etc/apt/apt.conf.d/20packagekit
 
-sudo apt install -y meld tig vim htop zsh tmux ranger sirikali
+sudo apt install -y meld tig vim htop zsh xclip tmux ranger sirikali
 
 # asdf ........................................................................
 
@@ -17,8 +17,8 @@ sudo apt-get install -y dirmngr gpg curl gawk
 
 # ... python
 sudo apt install -y build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev curl git \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+	libbz2-dev libreadline-dev libsqlite3-dev curl git \
+	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # ... ruby
 sudo apt install -y autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
