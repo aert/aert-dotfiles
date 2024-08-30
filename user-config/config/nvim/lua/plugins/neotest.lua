9755d64e60e1915c28ef4fb50ea56564bcc2bb3a
abdourakhmane <ripault.a@gmail.com>
2024-08-30 11:37:03 +0000

    nvim

---
+++ user-config/config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    lazy = true,
+    dependencies = {
+      "zidhuss/neotest-minitest",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-minitest"),
+        },
+      })
+    end,
+  },
+}
