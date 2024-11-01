8e015d02d9979948b62261b99bb43e25c2a86f1f
abdourakhmane <ripault.a@gmail.com>
2024-11-01 09:31:42 +0000

    lazyvim: remove frecency

--- user-config/config/nvim/lua/plugins/telescope.lua
+++ user-config/config/nvim/lua/plugins/telescope.lua
@@ -3,16 +3,6 @@ return {
   {
     "nvim-telescope/telescope.nvim",
 
-    dependencies = {
-      {
-        "nvim-telescope/telescope-frecency.nvim",
-        keys = {
-          { "<leader>fr", "<cmd>Telescope frecency<cr>", mode = "n", desc = "Recent" },
-          { "<leader>fR", "<cmd>Telescope frecency workspace=CWD<cr>", mode = "n", desc = "Recent (cwd)" },
-        },
-      },
-    },
-
     opts = function(_, opts)
       local actions = require("telescope.actions")
       opts.defaults = {
