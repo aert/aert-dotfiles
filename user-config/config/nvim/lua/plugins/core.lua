9f9bb37ef4c6640479bba4f8050e3cbb6a550969
abdourakhmane <ripault.a@gmail.com>
2025-02-18 09:34:20 +0000

    tmux, nvim transparency

--- user-config/config/nvim/lua/plugins/core.lua
+++ user-config/config/nvim/lua/plugins/core.lua
@@ -1,30 +1,68 @@
 -- https://www.lazyvim.org/configuration/recipes#make-tokyonight-transparent
 return {
   -- themes
-  { "ellisonleao/gruvbox.nvim" },
-  { "rebelot/kanagawa.nvim" },
-  { "sainnhe/everforest" },
-  { "EdenEast/nightfox.nvim" },
   {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    -- "folke/tokyonight.nvim",
-    "LazyVim/LazyVim",
-    opts = {
-      -- colorscheme = "gruvbox",
-      -- colorscheme = "catppuccin-mocha",
-      -- colorscheme = "murphy",
-      -- colorscheme = "desert",
-      -- colorscheme = "tokyonight-moon",
-      colorscheme = "kanagawa",
-      -- colorscheme = "vim",
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+    {
+      "tokyonight.nvim",
+      opts = {
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      },
+    },
+    {
+      "rebelot/kanagawa.nvim",
+      opts = {
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      },
+    },
+    {
+      "sainnhe/everforest",
+      config = function()
+        vim.g.everforest_transparent_background = 2
+        vim.g.everforest_float_style = "dim"
+        vim.g.everforest_diagnostic_text_highlight = 1
+        vim.g.everforest_diagnostic_line_highlight = 1
+        vim.g.everforest_diagnostic_virtual_text = "highlighted"
+        vim.g.everforest_dim_inactive_windows = 1
+        vim.g.everforest_ui_contrast = "high"
+      end,
+    },
+    { "EdenEast/nightfox.nvim" },
+    {
+      "scottmckendry/cyberdream.nvim",
+      lazy = false,
+      priority = 1000,
+      opts = {
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
+      },
+    },
+    {
+      -- "folke/tokyonight.nvim",
+      "LazyVim/LazyVim",
+      opts = {
+        -- colorscheme = "gruvbox",
+        -- colorscheme = "catppuccin-mocha",
+        -- colorscheme = "murphy",
+        -- colorscheme = "desert",
+        -- colorscheme = "tokyonight-moon",
+        colorscheme = "kanagawa",
+        -- colorscheme = "vim",
+        transparent = true,
+        styles = {
+          sidebars = "transparent",
+          floats = "transparent",
+        },
       },
     },
   },
