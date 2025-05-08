9a7f9b969e4393bf6753141d8c0a423a93b922cc
abdourakhmane <ripault.a@gmail.com>
2025-05-08 18:38:30 +0000

    nvim: jbuilder

--- user-config/config/nvim/lua/config/options.lua
+++ user-config/config/nvim/lua/config/options.lua
@@ -14,3 +14,10 @@ vim.g.autoformat = false
 -- vim.go.background = "light"
 
 vim.g.mapleader = ","
+
+-- support jbuilder files
+vim.filetype.add({
+  extension = {
+    jbuilder = 'ruby',
+  },
+})
